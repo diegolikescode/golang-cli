deddf2307f4fb47618f773cb8efddd4393c24b4c
Diego Prestes <diegoprestes.git@hotmail.com>
2021-05-20 00:10:52 -0300

    feat: my first command-line interface is ready

---
+++ cmd/random.go
@@ -0,0 +1,85 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"log"
+	"encoding/json"
+	"github.com/spf13/cobra"
+)
+
+// randomCmd represents the random command
+var randomCmd = &cobra.Command{
+	Use:   "random",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		getRandomJoke()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(randomCmd)
+
+}
+
+type Joke struct {
+	ID string `json:"id"`
+	Joke string `json:"joke"`
+	Status int `json:"status"`
+}
+
+func getRandomJoke() {
+	url := "http://icanhazdadjoke.com/"
+	responseBytes := getJokeData(url)
+	joke := Joke{}
+	
+	if err := json.Unmarshal(responseBytes, &joke); err != nil{
+		log.Printf("not get itXXXXX - %v", err)
+	}
+
+	fmt.Println(string(joke.Joke))
+}
+
+func getJokeData(baseAPI string) []byte {
+	request, err := http.NewRequest(http.MethodGet, baseAPI, nil)
+	if err != nil {
+		log.Printf("not get itX - %v", err)
+	}
+
+	request.Header.Add("Accept", "application/json")
+	request.Header.Add("User-Agent", "my-first-cli (github.com/DiegoPrestesGit/my-first-cli)")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		log.Printf("not get itXX - %v", err)
+	}
+
+	responseBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("not get itXXX - %v", err)
+	}
+
+	return responseBytes
+}
